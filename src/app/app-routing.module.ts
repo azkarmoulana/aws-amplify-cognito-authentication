6bc88b9233ae9e92f76e9a64688505afe722f7ae
azkarmoulana <azkar.moulana@gmail.com>
2019-11-04 23:55:10 +0530

    configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserAuthenticationComponent } from './user-authentication/user-authentication.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserAuthenticationComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
