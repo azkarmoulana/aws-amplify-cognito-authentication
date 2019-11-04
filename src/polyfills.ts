ddf1a3e30172f4450a0df8ab2b2334b5c525b277
azkarmoulana <azkar.moulana@gmail.com>
2019-11-04 23:54:59 +0530

    added polyfills for amplify

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,11 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+ /**
+  * AWS Amplify - Angular 6+ support
+  */
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
