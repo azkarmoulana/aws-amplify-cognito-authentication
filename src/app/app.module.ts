acc2863706934a018664d4f55b60e8e83d405460
azkarmoulana <azkar.moulana@gmail.com>
2019-11-04 23:55:50 +0530

    import amplify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserAuthenticationComponent } from './user-authentication/user-authentication.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAuthenticationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
